d13cc1456a3042be2a576248bfa9e1e0784799ed
soyidwahyud <soyidwahyu4@gmail.com>
2019-09-11 20:03:12 +0700

    Add Music player project

--- MusicPlayer/app/src/main/java/id/ac/polinema/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/id/ac/polinema/musicplayer/MainActivity.java
@@ -3,12 +3,21 @@ package id.ac.polinema.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView teks1, teks2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teks1 = (TextView)findViewById(R.id.teks1);
+        teks2 = (TextView)findViewById(R.id.teks2);
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "font/lato_light.ttf");
+        teks1.setTypeface(customFont);
+        Typeface customFont2 = Typeface.createFromAsset(getAssets(), "font/lato_regular.ttf");
+        teks2.setTypeface(customFont2);
     }
 }
